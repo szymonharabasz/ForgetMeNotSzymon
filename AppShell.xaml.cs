81dda2b64b2f930e664adab9e9a868b8ad894f83
Szymon Harabasz <szymon.harabasz@gmail.com>
2023-12-24 15:36:06 +0100

    Initial commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ForgetMeNotSzymon;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace ForgetMeNotSzymon;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        string timeOrTimes = count == 1 ? "time" : "times";
+
+        CounterBtn.Text = $"Clicked {count} {timeOrTimes}";
+        CounterLabel.Text = $"Button was clicked {count} {timeOrTimes}";
+        
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
